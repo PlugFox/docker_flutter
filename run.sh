3d780c43478897309b4b45a26c332188733826a9
FOX <plugfox@gmail.com>
2020-08-11 23:29:29 +0300

    rm run.sh

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -ex
-
-docker run --rm -it -v ${PWD}:/build --workdir /build plugfox/flutter:stable flutter doctor
-docker run --rm -it -v ${PWD}:/build --workdir /build plugfox/flutter:beta flutter doctor
